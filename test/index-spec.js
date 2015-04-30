fc1eae1f5f12e556ecb7ec6a27436460f4a09ddf
Nick Wronski <nick@codeschool.com>
2015-04-29 22:02:12 -0400

    updated specs for block comments, conditional block comments. refs #5

--- test/index-spec.js
+++ test/index-spec.js
@@ -5,19 +5,43 @@ var expect            = require('chai').expect
     tree              = require('./helpers.js');
 
 describe('html-tag-validator', function() {
-  it('basic-div', function(done) { 
-    tree.ok(this, done); 
+  it('basic div', function(done) {
+    tree.ok(this, done);
   });
 
-  // it('anchor-tags', function(done) { 
-  //   tree.ok(this, done); 
-  // });
+  it('anchor tags', function(done) {
+    tree.ok(this, done);
+  });
 
-  // it('basic-angular', function(done) { 
-  //   tree.ok(this, done); 
-  // });
+  it('basic angular', function(done) {
+    tree.ok(this, done);
+  });
 
-  // it('malformed-attribute', function (done) {
-  //   tree.error("Found an attribute assignment \"=\" not followed by a value", this, done);
-  // });
+  it('basic self closing', function(done) {
+    tree.ok(this, done);
+  });
+
+  it('malformed attribute', function (done) {
+    tree.error("Found an attribute assignment \"=\" not followed by a value", this, done);
+  });
+
+  it('html comment', function(done) {
+    tree.ok(this, done);
+  });
+
+  it('malformed comment', function(done) {
+    tree.error("Cannot have two or more consecutive hyphens inside of a block comment", this, done);
+  });
+
+  it('html comment conditional', function(done) {
+    tree.ok(this, done);
+  });
+
+  it('malformed conditional comment', function(done) {
+    tree.error("Conditional comment start tag found without conditional comment end tag", this, done);
+  });
+
+  it('malformed conditional comment 2', function(done) {
+    tree.error("Conditional comment end tag found without conditional comment start tag", this, done);
+  });
 });
