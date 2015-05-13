b98f9eb219e53149c95de8dfd4ebf21fbcf20bf4
Nick Wronski <nick@codeschool.com>
2015-05-13 17:15:13 -0400

    added ability to do deep equals against two HTML trees using tree.equals()

--- test/helpers.js
+++ test/helpers.js
@@ -3,29 +3,33 @@ var expect            = require('chai').expect,
     _                 = require('lodash'),
     htmlTagValidator  = require('../index'),
     prettyjson        = require('prettyjson'),
-    getTree, assertOkTree, assertErrorTree;
+    format, broadcast, isDefined, getTree, assertOkTree, assertErrorTree;
 
-var _canB,
-    canBroadcast = function () {
-      if (_canB == null) {
-        _canB = console != null && console.log != null &&
-                process != null && process.env != null &&
-                process.env.DEBUG != null;
+
+format = function (arg, ugly) {
+  return _.isObject(arg) || (_.isString(arg) && /^[\{\[].*[\{\[]$/.test(arg)) ?
+      (ugly ? JSON.stringify(arg) : prettyjson.render(arg, {})) :
+      arg;
+};
+
+broadcast = function (args) {
+  // Only broadcast when DEBUG=true is set in the environment
+  if (broadcast.canBroadcast) {
+    _.forEach(args, function (arg) {
+      if (isDefined(arg)) {
+        console.log('\n\n', format(arg), '\n');
       }
-      return _canB;
-    },
-    broadcast = function (args) {
-      // Only broadcast when DEBUG=true is set in the environment
-      if (canBroadcast()) {
-        _.forEach(args, function (arg) {
-          if (arg != null) {
-            console.log('\n');
-            console.log(_.isObject(arg) ? prettyjson.render(arg, {}) : arg);
-            console.log('\n');
-          }
-        });
-      }
-    };
+    });
+  }
+};
+
+broadcast.canBroadcast = console != null && console.log != null &&
+                          process != null && process.env != null &&
+                          process.env.DEBUG != null;
+
+isDefined = function (arg) {
+  return arg != null;
+};
 
 /**
 Load the source file for the current test and then try and generate the AST from it.
@@ -68,8 +72,8 @@ assertOkTree = function (that, done) {
     func = arguments[2];
   }
   getTree.apply(that, [that, options, function (err, ast) {
-    broadcast(arguments);
-    expect(err).to.be.null;
+    if (isDefined(err)) { broadcast(arguments); }
+    expect(err).to.not.be.ok;
     expect(ast).to.be.ok;
     func.call(that);
   }]);
@@ -96,10 +100,10 @@ assertErrorTree = function (obj, that, done) {
   if (_.isUndefined(obj)) { obj = {}; }
   else if (_.isString(obj)) { obj = { 'message': obj }; }
   getTree.apply(that, [that, options, function (err, ast) {
-    broadcast(arguments);
-    expect(ast).to.be.undefined;
+    if (isDefined(ast)) { broadcast(arguments); }
+    expect(ast).to.not.be.ok;
     if (obj == null) {
-      expect(err).to.not.be.null;
+      expect(err).to.be.ok;
     } else {
       _.forEach(obj, function (v, k) {
         expect(err).to.include.keys(k);
@@ -110,9 +114,39 @@ assertErrorTree = function (obj, that, done) {
   }]);
 };
 
+/**
+Assert that the current file returns an AST containing the properties and values in the
+given object.
+@note Alternative arguments format to pass options object is: obj, that, options, done.
+@param [Object|String] obj
+  The properties and values to assert within the AST generated for the running test.
+@param [Object] that
+  The context of the running test
+@param [Function] done
+  The function to call when the test is completed
+*/
+assertEqualsTree = function (obj, that, done) {
+  var options = {}, func = done;
+  if (arguments.length > 3) {
+    options = done;
+    func = arguments[3];
+  }
+  if (_.isUndefined(obj)) { obj = {}; }
+  getTree.apply(that, [that, options, function (err, ast) {
+    if (isDefined(err)) { broadcast(arguments); }
+    expect(err).to.not.be.ok;
+    if (_.isString(obj)) {
+      obj = JSON.parse(obj);
+    }
+    expect(ast).to.deep.equal(obj);
+    func.call(that);
+  }]);
+};
+
 module.exports = {
   'get': getTree,
   'ok': assertOkTree,
   'error': assertErrorTree,
+  'equals': assertEqualsTree,
   'broadcast': broadcast
 };
