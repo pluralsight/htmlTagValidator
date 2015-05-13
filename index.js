ead5d9f531e9b13f3c729e759d397322ae5fff59
Nick Wronski <nick@codeschool.com>
2015-05-13 17:16:32 -0400

    exported function can now work syncronously or asyncronously

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 var htmlParser = require('./src/html-parser');
 
 function htmlTagValidator(source, callback) {
-  var options = htmlTagValidator._options, func = callback;
+  var options = htmlTagValidator._options, func = callback,
+      isSync = (Object.prototype.toString.call(func) !== '[object Function]');
   if (arguments.length > 2) {
     options = callback;
     func = arguments[2];
   }
+
   if (Object.prototype.toString.call(func) !== '[object Function]') {
-    throw new TypeError(
-      'The last argument provided to the parser function should be a callback function',
-      htmlTagValidator._name
-    );
+    // Sync
+    return htmlParser(source, options);
   }
-  setTimeout(function () {
-	  try {
-	  	var ast = htmlParser.parse(source, options);
-      func(null, ast);
-	  } catch (e) {
-      func(e);
-	  }
-  }, 0);
+
+  // Async
+  setTimeout((function (ctx, s, o, f) {
+    return function () {
+      var ast, err;
+      try {
+        ast = htmlParser.parse(s, o);
+      } catch (e) {
+        err = e;
+      }
+      f.apply(ctx, [err, ast]);
+    };
+  })(this, source, options, func), 0);
 };
 
 htmlTagValidator._name = "html-tag-validator";
-htmlTagValidator.VERSION = "1.0.3";
+htmlTagValidator.VERSION = "1.0.6";
 htmlTagValidator._options = {};
 
 htmlTagValidator.setOptions = function(options) {
