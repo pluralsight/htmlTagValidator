014c90dd32e5e6a59e8fc37937ac71226e533346
Nick Wronski <nick@codeschool.com>
2015-05-14 14:57:12 -0400

    removing old code in .prev

--- .prev/htmlParser.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var PEG = require("pegjs");
-var htmlGrammar = fs.readFileSync(__dirname + "/htmlGrammar.pegjs", 'utf8');
-// fs.writeFileSync(__dirname + "/htmlParser.js", PEG.buildParser(htmlGrammar));
-module.exports = PEG.buildParser(htmlGrammar);
